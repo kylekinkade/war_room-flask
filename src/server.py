a1e7af24dd3023bc8c411079c9074486572b6a7f
Kyle Kinkade <kkinkade@wetpaint-inc.com>
2015-04-11 10:43:49 -0700

    adding server

---
+++ src/server.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, request, session
+from sqlite3 import dbapi2 as sqlite3
+
+app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'wargame.db'),
+    USERNAME='admin',
+    PASSWORD='RAWRR'
+))
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqli
+
+@app.route('/hello')
+def hello():
+    return "Hello there! c:"
