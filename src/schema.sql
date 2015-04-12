ee207f1486971ef4ad136707abd51df516dc66b1
Kyle Kinkade <kkinkade@wetpaint-inc.com>
2015-04-12 15:37:42 -0700

    rawrrrrrrr

--- src/schema.sql
+++ src/schema.sql
@@ -5,14 +5,14 @@ create table players (
   population integer not null
 );
 
-drop table if exists players_cards
+drop table if exists players_cards;
 create table players_cards (
     id integer primary key autoincrement,
     card_id integer not null,
     player_id integer not null
 );
 
-drop table if exists moves
+drop table if exists moves;
 create table moves (
     id integer primary key autoincrement,
     card_id integer not null,
